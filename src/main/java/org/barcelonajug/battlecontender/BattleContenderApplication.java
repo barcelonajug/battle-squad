64c985590bcf08d1c0f87c244d280beb71dc200b
Anyul Rivas <anyulled@gmail.com>
2026-06-29 23:17:54 +0200

    feat: load environment variables from .env file during application startup

--- src/main/java/org/barcelonajug/battlecontender/BattleContenderApplication.java
+++ src/main/java/org/barcelonajug/battlecontender/BattleContenderApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BattleContenderApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BattleContenderApplication.class, args);
     }
 }
